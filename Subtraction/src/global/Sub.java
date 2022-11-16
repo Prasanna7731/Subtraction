3bc18023d2bf09b29760900ba150524e035bda75
Prasanna7731 <prasannakumar7tata@gmail.com>
2022-11-16 14:43:51 +0530

    Source code

---
+++ Subtraction/src/global/Sub.java
@@ -0,0 +1,14 @@
+package global;
+
+public class Sub {
+
+	public static void main(String[] args) {
+
+           int a=10;
+           int b=5;
+           int c=a-b;
+           System.out.println(c);
+
+	}
+
+}
